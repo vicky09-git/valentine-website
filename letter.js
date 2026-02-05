30d7e0b822bf37b0fe7ee25936e546a38d94e579
vicky09-git <aggrwal.vicky@gmail.com>
2026-02-05 12:38:19 +0530

    Update letter.js

--- letter.js
+++ letter.js
@@ -1,19 +1,11 @@
 const noBtn = document.getElementById("no");
-const buttonsBox = document.querySelector(".buttons");
 
-noBtn.style.position = "absolute";
+noBtn.addEventListener("mouseover", () => {
+  noBtn.style.position = "absolute";
+  noBtn.style.left = Math.random() * 200 + "px";
+  noBtn.style.top = Math.random() * 200 + "px";
+});
 
-noBtn.addEventListener("mouseenter", () => {
-  const padding = 10;
-
-  const maxX =
-    buttonsBox.clientWidth - noBtn.offsetWidth - padding;
-  const maxY =
-    buttonsBox.clientHeight - noBtn.offsetHeight - padding;
-
-  const x = Math.random() * maxX;
-  const y = Math.random() * maxY;
-
-  noBtn.style.left = x + "px";
-  noBtn.style.top = y + "px";
+document.getElementById("yes").addEventListener("click", () => {
+  document.body.innerHTML = "<h1 style='color:#d62828'>YAYYY ❤️</h1>";
 });
