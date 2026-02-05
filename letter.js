db551b4e5791c35a5f29a69d1d11a317cc395d9e
vicky09-git <aggrwal.vicky@gmail.com>
2026-02-05 12:31:03 +0530

    Update letter.js

--- letter.js
+++ letter.js
@@ -4,25 +4,20 @@
 const noBtn = document.getElementById("no");
 const buttonsBox = document.querySelector(".buttons");
 
-// force absolute inside buttons
+// lock vertical position
 noBtn.style.position = "absolute";
-noBtn.style.left = "10px";
 noBtn.style.top = "10px";
 
 noBtn.addEventListener("mouseenter", () => {
-  const padding = 5;
-
+  const padding = 10;
   const maxX =
     buttonsBox.clientWidth - noBtn.offsetWidth - padding;
-  const maxY =
-    buttonsBox.clientHeight - noBtn.offsetHeight - padding;
 
   const x = Math.random() * maxX;
-  const y = Math.random() * maxY;
 
   noBtn.style.left = x + "px";
-  noBtn.style.top = y + "px";
 });
+
 
 
 // =====================
