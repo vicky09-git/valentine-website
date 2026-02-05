d5a71b90c0cd8afe4184a672e0df8f0aa25edaf3
vicky09-git <aggrwal.vicky@gmail.com>
2026-02-06 00:21:37 +0530

    Update letter.js

--- letter.js
+++ letter.js
@@ -1,18 +1,1 @@
-const noBtn = document.getElementById("no");
-const container = document.querySelector(".content"); // main box
 
-noBtn.style.position = "absolute";
-
-noBtn.addEventListener("mouseenter", () => {
-  const containerRect = container.getBoundingClientRect();
-  const btnRect = noBtn.getBoundingClientRect();
-
-  const maxX = containerRect.width - btnRect.width;
-  const maxY = containerRect.height - btnRect.height;
-
-  const randomX = Math.random() * maxX;
-  const randomY = Math.random() * maxY;
-
-  noBtn.style.left = `${randomX}px`;
-  noBtn.style.top = `${randomY}px`;
-});
